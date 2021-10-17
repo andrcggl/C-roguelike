5cec18ccd9f9215117021051c3f30ab4ff381d14
andrcggl <andrecoggiola@gmail.com>
2021-10-17 20:52:30 -0300

    first commit

---
+++ include/rogue.h
@@ -0,0 +1,19 @@
+#ifndef ROGUE_H
+#define ROGUE_H
+
+#include <ncurses.h>
+#include <stdlib.h>
+
+typedef struct
+{
+  int y;
+  int x;
+} Position;
+
+typedef struct
+{
+  Position pos;
+  char ch;
+} Entity;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <rogue.h>
+
+int main(void)
+{
+  initscr();
+  noecho();
+  curs_set(0);
+
+
+  while(getch() != 'q')
+  {
+    mvaddch(10, 20, '@');
+  }
+
+  endwin();
+
+  return 0;
+}
